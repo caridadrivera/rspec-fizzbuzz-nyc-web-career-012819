4b1941d2a2b682138c50492259549f36d5d976c5
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-15 01:47:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number) 
+  if (number == number % 3)
+    return "Fizz"
+  elsif (number == number % 3) && (number == number % 5)
+    return "FizzBuzz"
+  else 
+    puts ""
+  end
+end

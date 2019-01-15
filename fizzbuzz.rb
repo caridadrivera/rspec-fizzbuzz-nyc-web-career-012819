3825d0b4da548005402c84fc962478ce419ff66c
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-15 01:50:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number) 
-  if (number % 3 == 0)
+  if number % 3 == 0
     return "Fizz"
-  elsif (number % 3 == 0) && (number % 5 == 0)
+  elsif number % 5 == 0
+    return "Buzz"
+  elsif number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
   else 
     puts ""
